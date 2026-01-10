ed9e544e9ab2751e9ab7ac1e132faf422f8768f5
Marvin <web@kkmarv.dev>
2026-01-10 17:52:05 +0100

    rm custom kn function

--- bash/.bashrc.d/functions.bash
+++ bash/.bashrc.d/functions.bash
@@ -9,21 +9,3 @@ aliases() {
     sed -nE 's/^alias ([^=]+)=.*/\1/p' "$file" | sort
   fi
 }
-
-kn() { # TODO test the function
-  local ns="$1"
-
-  if [[ -z "$ns" ]]; then
-    echo "usage: kn <namespace>" >&2
-    return 1
-  fi
-
-  if ! kubectl auth can-i get pods -n "$ns" >/dev/null 2>&1; then
-    echo "no access to namespace '$ns' (or it does not exist)" >&2
-    return 1
-  fi
-
-  kubectl config set-context --current --namespace="$ns" >/dev/null
-
-  echo "switched to namespace '$ns' in context $(kubectl config current-context)"
-}
