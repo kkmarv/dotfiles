342b48b8adea24f5eacc9dbd69ba09010312c3c6
Marvin <web@kkmarv.dev>
2026-01-10 17:45:26 +0100

    add krew, yq, jq

--- bash/.bashrc
+++ bash/.bashrc
@@ -25,3 +25,6 @@ fi
 export NVM_DIR="$XDG_CONFIG_HOME/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Load krew
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -5,7 +5,7 @@ alias h='helm'
 alias hf='helmfile'
 
 alias k='kubectl'
-alias kc='kubectl config use-context'
+alias kd='kubectl run netshoot --image=nicolaka/netshoot -i --tty --rm'
 
 alias g='git'
 alias ga='git add'
@@ -14,14 +14,16 @@ alias gd='git diff'
 alias gs='git status'
 
 # Wrapper
-alias ..='cd ..'
 alias ...='cd ../..'
-alias dmesg='dmesg --color=always'
-alias ip='ip --color'
-alias less='less -R'
+alias ..='cd ..'
+alias dmesg='dmesg -L=always'
+alias ip='ip -c'
+alias jq='jq -C'
+alias less='less -RF'
 alias ll='ls -lAh'
 alias ls='ls --color'
 alias mkdir='mkdir -p'
+alias yq='yq -C'
 
 # Utility
 alias week='date +%V'
