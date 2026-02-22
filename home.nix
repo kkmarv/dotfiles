ef5cac1a0eadf0c50ca42e594ae84c6826cded22
Marvin <web@kkmarv.dev>
2026-02-22 19:36:45 +0100

    apply nixfmt

--- home.nix
+++ home.nix
@@ -1,16 +1,10 @@
-{lib, pkgs, ...}:
-{
+{ lib, pkgs, ... }: {
   home = {
-    packages = with pkgs; [
-      hello
-      cowsay lolcat
-      home-manager
-    ];
+    packages = with pkgs; [ home-manager nixfmt cowsay lolcat ];
 
     username = "marvin";
     homeDirectory = "/home/marvin";
 
-  
     # You do not need to change this if you're reading this in the future.
     # Don't ever change this after the first build.  Don't ask questions.
     stateVersion = "23.11";
