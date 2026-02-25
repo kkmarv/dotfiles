90f091cdb6b453f61d91605f209e0d5a020b7986
Marvin <web@kkmarv.dev>
2026-02-25 01:46:21 +0100

    add nixfmt-tree

--- flake.nix
+++ flake.nix
@@ -7,12 +7,15 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs =
+    { nixpkgs, home-manager, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in {
+    in
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
       homeConfigurations.default = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./home.nix ];
--- home.nix
+++ home.nix
@@ -11,7 +11,6 @@
 
   home.packages = with pkgs; [
     home-manager
-    nixfmt
     cowsay
     lolcat
     htop
