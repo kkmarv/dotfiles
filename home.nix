f077465bed85d35960c759da4095afcc0c07b1a5
Marvin <web@kkmarv.dev>
2026-02-25 01:46:53 +0100

    migrate direnv + git

--- home.nix
+++ home.nix
@@ -17,11 +17,9 @@
     jq
     yq
     bat
-    direnv
     just
   ];
 
-  # programs.home-manager.enable = true;
   programs.bash.enable = true;
   programs.bash.shellAliases = {
     c = "clear";
@@ -55,6 +53,51 @@
     week = "date +%V";
     reload = "exec \${SHELL} -l";
     ipinfo = ''echo "Internal: $(ip route get 1.1.1.1 | awk "{print $7}")"; echo "External: $(curl -s https://ifconfig.me)"'';
+  };
+
+  programs.git.enable = true;
+  programs.git.ignores = [ ".env" ];
+  programs.git.settings = {
+    # user.email = "web@kkmarv.dev";
+    # user.name = "Marvin";
+    branch.sort = "-committerdate";
+    core.commentChar = ";";
+    credential.helper = "store";
+    init.defaultBranch = "main";
+    alias = {
+      plog = "log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative";
+      # Show colored log with graph and relative timestamps.
+
+      fpush = "push --force-with-lease";
+      # Force-push safely.
+
+      undo = "reset --soft HEAD";
+      # Undo the latest commit.
+
+      tags = "tag -l";
+      branches = "branch --all";
+      remotes = "remote --verbose";
+      # Show verbose output about tags, branches or remotes.
+
+      aliases = "config --get-regexp alias";
+      # List aliases.
+
+      amend = "commit --amend --reuse-message=HEAD";
+      # Amend the currently staged files to the latest commit.
+
+      contributors = "shortlog --summary --numbered";
+      # List contributors with number of commits.
+
+      whoami = "config user.email";
+      # Show the user email for the current repository.
+    };
+  };
+
+  programs.direnv.enable = true;
+  # programs.direnv.nix-direnv.enable = true; TODO check this out
+  programs.direnv.config = {
+    global.load_dotenv = true;
+    whitelist.prefix = [ "/home/marvin/dev" ];
   };
 
   programs.starship.enable = true;
